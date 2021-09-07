02bb4db66215194a9b1239d53fe4c048933b42ec
Thomas Astleitner <thomas.astleitner@bachmann.info>
2021-09-07 12:22:04 +0200

    finished and simulator tested, verification at home necessary

--- nodes/powerswitch.js
+++ nodes/powerswitch.js
@@ -1,32 +1,57 @@
 module.exports = function(RED) {
 	function PowerSwitchNode(config) {
 		RED.nodes.createNode(this,config);
+		var context = this.context;
 		var nodeThis = this;
 		var err = false;
-		var timeoutHandle, timeoutValue;
-		var msgCmd = null;
+		var timeoutHandle;
+		var msgCmd, msgDebug = null;
 
 		this.on('input', function(msg,send,done) {
 
-			config.buttonTopic = config.buttonTopic || "button";
+			config.toggleTopic = config.toggleTopic || "toggle";
 			config.feedbackTopic = config.feedbackTopic || "feedback";
 			config.forceTopic = config.forceTopic || "force";
-			
+
+			// convert config strings
+			if (config.togglePayloadType === 'num') {context.togglePayload = Number(config.togglePayload)}
+				else if (config.togglePayloadType === 'bool') {context.togglePayload = config.togglePayload === 'true'}
+				else {context.togglePayload = config.togglePayload}
+			;
+
+			if (config.feedbackPayloadOnType === 'num') {context.feedbackPayloadOn = Number(config.feedbackPayloadOn)}
+				else if (config.feedbackPayloadOnType === 'bool') {context.feedbackPayloadOn = config.feedbackPayloadOn === 'true'}
+				else {context.feedbackPayloadOn = config.feedbackPayloadOn}
+			;
+
+			if (config.feedbackPayloadOffType === 'num') {context.feedbackPayloadOff = Number(config.feedbackPayloadOff)}
+				else if (config.feedbackPayloadOffType === 'bool') {context.feedbackPayloadOff = config.feedbackPayloadOff === 'true'}
+				else {context.feedbackPayloadOff = config.feedbackPayloadOff}
+			;
+
+			if (config.forcePayloadOnType === 'num') {context.forcePayloadOn = Number(config.forcePayloadOn)}
+				else if (config.forcePayloadOnType === 'bool') {context.forcePayloadOn = config.forcePayloadOn === 'true'}
+				else {context.forcePayloadOn = config.forcePayloadOn}
+			;
+
+			if (config.forcePayloadOffType === 'num') {context.forcePayloadOff = Number(config.forcePayloadOff)}
+				else if (config.forcePayloadOffType === 'bool') {context.forcePayloadOff = config.forcePayloadOff === 'true'}
+				else {context.forcePayloadOff = config.forcePayloadOff}
+			;
+
+			// convert timeout variables
 			if (config.timeoutActive && config.timeoutUnit === "h") {
-				timeoutValue = config.timeoutValue * 3600000
+				context.timeoutValue = config.timeoutValue * 3600000
 			} else if (config.timeoutActive && config.timeoutUnit === "m") {
-				timeoutValue = config.timeoutValue * 60000
+				context.timeoutValue = config.timeoutValue * 60000
 			} else if (config.timeoutActive && config.timeoutUnit === "s") {
-				timeoutValue = config.timeoutValue * 1000
+				context.timeoutValue = config.timeoutValue * 1000
 			} else {
-				timeoutValue = 0
+				context.timeoutValue = 0
 			}
 			
 			function timeoutFunc() {
-				sendMsgCmdFunc(nodeThis.context.lightSetOn = false);
-				if (!config.feedbackActive) {
-					setNodeState();
-				}
+				sendMsgCmdFunc(context.lightSetOn = false);
 			}
 
 			function sendMsgCmdFunc(command) {
@@ -36,42 +61,52 @@ module.exports = function(RED) {
 				}
 				nodeThis.send(msgCmd);
 				if (!config.feedbackActive) {
-					nodeThis.context.lightIsOn = command;
-					setNodeState();
+					context.lightIsOn = command;
 				}
+				setNodeState();
 			}
 
 			function setNodeState() {
-				if (nodeThis.context.lightIsOn && !nodeThis.context.lightSetOn) {
+				if (context.lightIsOn && !context.lightSetOn) {
 					nodeThis.status({fill:"red",shape:"ring",text:"powering off"});
-				} else if (nodeThis.context.lightIsOn && nodeThis.context.lightSetOn) {
+				} else if (context.lightIsOn && context.lightSetOn) {
 					nodeThis.status({fill:"green",shape:"dot",text:"on"});
-					if (timeoutValue > 0) {
-						timeoutHandle = setTimeout(timeoutFunc, timeoutValue);
+					if (context.timeoutValue > 0) {
+						timeoutHandle = setTimeout(timeoutFunc, context.timeoutValue);
 					}
-				} else if (!nodeThis.context.lightIsOn && !nodeThis.context.lightSetOn) {
+				} else if (!context.lightIsOn && !context.lightSetOn) {
 					nodeThis.status({fill:"red",shape:"dot",text:"off"});
 					clearTimeout(timeoutHandle);
-				} else if (!nodeThis.context.lightIsOn && nodeThis.context.lightSetOn) {
+				} else if (!context.lightIsOn && context.lightSetOn) {
 					nodeThis.status({fill:"green",shape:"ring",text:"powering on"});
 				}
 			}
 
-			if (msg.topic === config.toggleTopic && msg.payload === config.toggleMessage) {
-				sendMsgCmdFunc(nodeThis.context.lightSetOn = !nodeThis.context.lightIsOn);
-			} else if (msg.topic === config.forceTopic && msg.payload === config.forceMessageOn) {
-				sendMsgCmdFunc(nodeThis.context.lightSetOn = true);
-			} else if (msg.topic === config.forceTopic && msg.payload === config.forceMessageOff) {
-				sendMsgCmdFunc(nodeThis.context.lightSetOn = false);
-			} else if (config.feedbackActive && msg.topic === config.feedbackTopic && msg.payload === config.feedbackMessageOn) {
-				nodeThis.context.lightIsOn = true;
-				nodeThis.context.lightSetOn = true;
-			} else if (config.feedbackActive && msg.topic === config.feedbackTopic && msg.payload === config.feedbackMessageOff) {
-				nodeThis.context.lightIsOn = false;
-				nodeThis.context.lightSetOn = false;
+			if (msg.topic === config.toggleTopic && msg.payload === context.togglePayload) {
+				sendMsgCmdFunc(context.lightSetOn = !context.lightIsOn);
+			} else if (msg.topic === config.forceTopic && msg.payload === context.forcePayloadOn) {
+				sendMsgCmdFunc(context.lightSetOn = true);
+			} else if (msg.topic === config.forceTopic && msg.payload === context.forcePayloadOff) {
+				sendMsgCmdFunc(context.lightSetOn = false);
+			} else if (config.feedbackActive && msg.topic === config.feedbackTopic && msg.payload === context.feedbackPayloadOn) {
+				context.lightIsOn = true;
+				context.lightSetOn = true;
+			} else if (config.feedbackActive && msg.topic === config.feedbackTopic && msg.payload === context.feedbackPayloadOff) {
+				context.lightIsOn = false;
+				context.lightSetOn = false;
 			}
 
 			setNodeState();
+
+			if (msg.debug) {
+				msgDebug = {
+					topic: "debug",
+					msg: msg,
+					config: config,
+					context: context
+				}
+				nodeThis.send(msgDebug);
+			}
 
 			if (err) {
 				if (done) {
@@ -83,7 +118,7 @@ module.exports = function(RED) {
 				}
 			}
 
-			this.context = nodeThis.context;
+			this.context = context;
 
 		});
 
