7ca7f2e0f2be91668d065a1644282461a077b63b
Thomas Astleitner <thomas.astleitner@bachmann.info>
2021-09-11 20:25:41 +0200

    changed from on to motion detector

--- nodes/powerswitch.js
+++ nodes/powerswitch.js
@@ -14,37 +14,47 @@ module.exports = function(RED) {
 		var context = this.context();
 		var nodeThis = this;
 		var err = false;
-		var absTimeoutHandle, onTimeoutHandle;
+		var absTimeoutHandle, motionTimeoutHandle;
 		var msgCmd, msgDebug = null;
 
 		this.on('input', function(msg,send,done) {
 
 			config.toggleTopic = config.toggleTopic || "toggle";
-			config.onTopic = config.onTopic || "on";
+			config.motionTopic = config.motionTopic || "motion";
 			config.feedbackTopic = config.feedbackTopic || "feedback";
 			config.forceTopic = config.forceTopic || "force";
 
-			// convert config strings
+			// convert config string: toggle payload
 			if (config.togglePayloadType === 'num') {context.togglePayload = Number(config.togglePayload)}
 			else if (config.togglePayloadType === 'bool') {context.togglePayload = config.togglePayload === 'true'}
 			else {context.togglePayload = config.togglePayload};
 
-			if (config.onPayloadType === 'num') {context.onPayload = Number(config.onPayload)}
-			else if (config.onPayloadType === 'bool') {context.onPayload = config.onPayload === 'true'}
-			else {context.onPayload = config.onPayload};
+			// convert config string: motion on payload
+			if (config.motionPayloadOnType === 'num') {context.motionPayloadOn = Number(config.motionPayloadOn)}
+			else if (config.motionPayloadOnType === 'bool') {context.motionPayloadOn = config.motionPayloadOn === 'true'}
+			else {context.motionPayloadOn = config.motionPayloadOn};
 
+			// convert config string: motion off payload
+			if (config.motionPayloadOffType === 'num') {context.motionPayloadOff = Number(config.motionPayloadOff)}
+			else if (config.motionPayloadOffType === 'bool') {context.motionPayloadOff = config.motionPayloadOff === 'true'}
+			else {context.motionPayloadOff = config.motionPayloadOff};
+
+			// convert config string: feedback on payload
 			if (config.feedbackPayloadOnType === 'num') {context.feedbackPayloadOn = Number(config.feedbackPayloadOn)}
 			else if (config.feedbackPayloadOnType === 'bool') {context.feedbackPayloadOn = config.feedbackPayloadOn === 'true'}
 			else {context.feedbackPayloadOn = config.feedbackPayloadOn};
 
+			// convert config string: feedback off payload
 			if (config.feedbackPayloadOffType === 'num') {context.feedbackPayloadOff = Number(config.feedbackPayloadOff)}
 			else if (config.feedbackPayloadOffType === 'bool') {context.feedbackPayloadOff = config.feedbackPayloadOff === 'true'}
 			else {context.feedbackPayloadOff = config.feedbackPayloadOff};
 
+			// convert config string: force on payload
 			if (config.forcePayloadOnType === 'num') {context.forcePayloadOn = Number(config.forcePayloadOn)}
 			else if (config.forcePayloadOnType === 'bool') {context.forcePayloadOn = config.forcePayloadOn === 'true'}
 			else {context.forcePayloadOn = config.forcePayloadOn};
 
+			// convert config string: force off payload
 			if (config.forcePayloadOffType === 'num') {context.forcePayloadOff = Number(config.forcePayloadOff)}
 			else if (config.forcePayloadOffType === 'bool') {context.forcePayloadOff = config.forcePayloadOff === 'true'}
 			else {context.forcePayloadOff = config.forcePayloadOff};
@@ -55,11 +65,11 @@ module.exports = function(RED) {
 			else if (config.absTimeoutActive && config.absTimeoutUnit === "s") {context.absTimeoutValue = config.absTimeoutValue * 1000}
 			else {context.absTimeoutValue = 0}
 			
-			// convert onTimeout variables
-			if (config.onTimeoutUnit === "h") {context.onTimeoutValue = config.onTimeoutValue * 3600000}
-			else if (config.onTimeoutUnit === "m") {context.onTimeoutValue = config.onTimeoutValue * 60000}
-			else if (config.onTimeoutUnit === "s") {context.onTimeoutValue = config.onTimeoutValue * 1000}
-			else {context.onTimeoutValue = 0}
+			// convert motionTimeout variables
+			if (config.motionTimeoutUnit === "h") {context.motionTimeoutValue = config.motionTimeoutValue * 3600000}
+			else if (config.motionTimeoutUnit === "m") {context.motionTimeoutValue = config.motionTimeoutValue * 60000}
+			else if (config.motionTimeoutUnit === "s") {context.motionTimeoutValue = config.motionTimeoutValue * 1000}
+			else {context.motionTimeoutValue = 0}
 			
 			function timeoutFunc() {
 				sendMsgCmdFunc(context.lightSetOn = false);
@@ -97,31 +107,33 @@ module.exports = function(RED) {
 			if (msg.topic === config.toggleTopic && msg.payload === context.togglePayload) {
 				sendMsgCmdFunc(context.lightSetOn = !context.lightIsOn);
 				context.lockedOn = context.lightSetOn;
-				clearTimeout(onTimeoutHandle);
-				// message: on with timer
-			} else if (msg.topic === config.onTopic && msg.payload === context.onPayload && !context.lockedOn) {
+				clearTimeout(motionTimeoutHandle);
+			// message: motion on
+			} else if (msg.topic === config.motionTopic && msg.payload === context.motionPayloadOn && !context.lockedOn) {
 				sendMsgCmdFunc(context.lightSetOn = true);
-				onTimeoutHandle = setTimeout(timeoutFunc, context.onTimeoutValue);
-				// message: force on
+			// message: motion off
+			} else if (msg.topic === config.motionTopic && msg.payload === context.motionPayloadOff && !context.lockedOn) {
+				motionTimeoutHandle = setTimeout(timeoutFunc, context.motionTimeoutValue);
+			// message: force on
 			} else if (msg.topic === config.forceTopic && msg.payload === context.forcePayloadOn) {
 				sendMsgCmdFunc(context.lightSetOn = true);
 				context.lockedOn = true;
-				clearTimeout(onTimeoutHandle);
-				// message: force off
+				clearTimeout(motionTimeoutHandle);
+			// message: force off
 			} else if (msg.topic === config.forceTopic && msg.payload === context.forcePayloadOff) {
 				sendMsgCmdFunc(context.lightSetOn = false);
 				context.lockedOn = false;
-				clearTimeout(onTimeoutHandle);
-				// message: feedback on
+				clearTimeout(motionTimeoutHandle);
+			// message: feedback on
 			} else if (config.feedbackActive && msg.topic === config.feedbackTopic && msg.payload === context.feedbackPayloadOn) {
 				context.lightIsOn = true;
 				context.lightSetOn = true;
-				// message: feedback off
+			// message: feedback off
 			} else if (config.feedbackActive && msg.topic === config.feedbackTopic && msg.payload === context.feedbackPayloadOff) {
 				context.lightIsOn = false;
 				context.lightSetOn = false;
 				context.lockedOn = false;
-				clearTimeout(onTimeoutHandle);
+				clearTimeout(motionTimeoutHandle);
 			}
 
 			setNodeState();
